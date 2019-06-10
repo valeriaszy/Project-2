d45960595ed82c46293a83f3a834cff08314f9f9
Anh Dao <addao823@gmail.com>
2019-06-09 19:05:34 -0700

    Add api routes get and post for recipe,ingredients

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,38 @@
+/* eslint-disable prettier/prettier */
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  ///Recipe
+  //return all recipe or via id
+  app.get("/api/recipe/:id?", function(req, res) {
+    var query = {};
+    if (req.params.id) {
+      query[where] = { id: req.params.id };
+    }
+
+    db.Recipe.findAll(querry).then(function(results) {
+      res.json(results);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  ///Ingredients
+  //return all ingredients or via id
+  app.get("api/ingredients/:id?", function(req, res) {
+    var querry = {};
+    if (req.params.id) {
+      query[where] = { id: req.params.id };
+    }
+
+    db.Ingredient.findAll(querry).then(function(results) {
+      res.json(results);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("api/ingredients/"), function(req, res) {
+    db.Ingredient.create({
+      name: req.body.name
+    }).then(function(result) {
+      res.json(result);
     });
-  });
+  };
 };
