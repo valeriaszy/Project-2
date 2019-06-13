3ee47627adf458699c331962bdb33a00b1713011
HowardKuo <howardkuo91@gmail.com>
2019-06-12 21:19:52 -0700

    jquery fix

---
+++ seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO Ingredient (name, description)
+VALUES ("Common Onion", "A vegetable from the Allum genus.");
+
+INSERT INTO Ingredient (name, description)
+VALUES ("Red Onion", "A vegetable from the Allum genus.");
+
+INSERT INTO Ingredient (name, description)
+VALUES ("Green Onion", "A vegetable from the Allum genus.")
+
+INSERT INTO Recipe (name, description, instructions)
+VALUES ("Onion Slaw", "Onions", "onions, and more onions", "Chop all the onions into fine pieces. Dump them into a bowl and eat.");
+
+INSERT INTO RecipeIngredients (quantity, unitOfMeasure)
+VALUES (3, "Units");
+
+INSERT INTO RecipeIngredients (quantity, unitOfMeasure)
+VALUES (2, "Units");
+
+INSERT INTO RecipeIngredients (quantity, unitOfMeasure)
+VALUES (1, "Units");
